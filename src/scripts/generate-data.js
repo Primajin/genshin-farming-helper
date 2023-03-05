bb1373a9911e2ed83f7e119428511e106ba7bdf1
Jannis Hell <Primajin@users.noreply.github.com>
2023-03-05 12:25:38 +0100

    :alien: Update code due to external code changes

--- src/scripts/generate-data.js
+++ src/scripts/generate-data.js
@@ -2,20 +2,22 @@ import fs from 'node:fs';
 import genshinDb from 'genshin-db';
 
 const defaultOptions = {matchCategories: true, verboseCategories: true};
-const sundayDrops = genshinDb.materials('Sunday', defaultOptions).sort((a, b) => a.sortorder - b.sortorder);
-const talentMaterials = sundayDrops.filter(material => material?.materialtype.startsWith('Talent'));
-const weaponMaterials = sundayDrops.filter(material => material?.materialtype.startsWith('Weapon'));
-const characterMaterials = genshinDb.materials('names', defaultOptions).filter(material => material?.materialtype.startsWith('Character Level-Up Material') || material?.materialtype.startsWith('Local')).sort((a, b) => a.sortorder - b.sortorder);
-const characterAscensionMaterials = characterMaterials.filter(material => material?.description.startsWith('Character Ascension'));
-const characterLVLMaterials = characterMaterials.filter(material => !material?.description.startsWith('Character Ascension'));
-const localSpecialties = characterMaterials.filter(material => material?.materialtype.startsWith('Local'));
+const talentMaterials = genshinDb.materials('Character Talent Material', defaultOptions).filter(material => !material?.name.startsWith('Crown')).sort((a, b) => a.sortorder - b.sortorder);
+const weaponMaterials = genshinDb.materials('Weapon Ascension Material', defaultOptions).sort((a, b) => a.sortorder - b.sortorder);
+const characterAscensionMaterials = genshinDb.materials('Character Ascension Material', defaultOptions);
+const characterWeaponEnhancementMaterialNames = new Set(genshinDb.crafts('Character and Weapon Enhancement Material', defaultOptions).map(material => material?.name));
+const allMaterials = genshinDb.materials('names', defaultOptions);
+const characterWeaponEnhancementMaterials = allMaterials.filter(material => characterWeaponEnhancementMaterialNames.has(material?.name));
+const characterLVLMaterials = allMaterials.filter(material => material?.materialtype.startsWith('Character Level-Up Material') && !material?.description.startsWith('Character Ascension')).sort((a, b) => a.sortorder - b.sortorder);
+const localSpecialties = allMaterials.filter(material => material?.materialtype.startsWith('Local'));
 
 const materials = {
-	talentMaterials,
-	weaponMaterials,
 	characterAscensionMaterials,
 	characterLVLMaterials,
+	characterWeaponEnhancementMaterials,
 	localSpecialties,
+	talentMaterials,
+	weaponMaterials,
 };
 
 fs.writeFile('src/data.json', JSON.stringify(materials), error => {
@@ -27,17 +29,18 @@ fs.writeFile('src/data.json', JSON.stringify(materials), error => {
 const talentMaterialsRare = materials.talentMaterials.filter(material => Number.parseInt(material?.rarity, 10) > 3);
 const weaponMaterialsRare = materials.weaponMaterials.filter(material => Number.parseInt(material?.rarity, 10) > 4);
 const characterAscensionMaterialsRare = materials.characterAscensionMaterials.filter(material => Number.parseInt(material?.rarity, 10) > 4);
-const characterLVLMaterialsRare = materials.characterLVLMaterials.filter(material => {
+const characterWeaponEnhancementMaterialsRare = materials.characterWeaponEnhancementMaterials.filter(material => {
 	const rarityInt = Number.parseInt(material?.rarity, 10);
 	return rarityInt > 2 ? (rarityInt === 3 ? !material.source.includes('Stardust Exchange') : true) : false;
 });
 
 const materialsRare = {
+	characterAscensionMaterials: characterAscensionMaterialsRare,
+	characterLVLMaterials,
+	characterWeaponEnhancementMaterials: characterWeaponEnhancementMaterialsRare,
+	localSpecialties,
 	talentMaterials: talentMaterialsRare,
 	weaponMaterials: weaponMaterialsRare,
-	characterAscensionMaterials: characterAscensionMaterialsRare,
-	characterLVLMaterials: characterLVLMaterialsRare,
-	localSpecialties,
 };
 
 fs.writeFile('src/data-rare.json', JSON.stringify(materialsRare), error => {
