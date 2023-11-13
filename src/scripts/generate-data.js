e0de677893c97ddd57639205097e46b9be6c6026
Jannis Hell <Primajin@users.noreply.github.com>
2023-11-13 17:51:36 +0100

    :relaxed: make material filtering more consistent

--- src/scripts/generate-data.js
+++ src/scripts/generate-data.js
@@ -2,16 +2,16 @@ import fs from 'node:fs';
 import genshinDb from 'genshin-db';
 
 const defaultOptions = {matchCategories: true, verboseCategories: true};
-const talentMaterials = genshinDb.materials('Character Talent Material', defaultOptions).filter(material => !material?.name.startsWith('Crown')).sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const weaponMaterials = genshinDb.materials('Weapon Ascension Material', defaultOptions).sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const characterAscensionMaterials = genshinDb.materials('Character Ascension Material', defaultOptions).sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const characterWeaponEnhancementMaterialNames = new Set(genshinDb.materials('Character and Weapon Enhancement Material', defaultOptions).map(material => material?.name));
-const allMaterials = genshinDb.materials('names', defaultOptions);
-const characterWeaponEnhancementMaterials = allMaterials.filter(material => characterWeaponEnhancementMaterialNames.has(material?.name)).sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const characterLVLMaterials = allMaterials.filter(material => material?.typeText.startsWith('Character Level-Up Material') && !material?.description.startsWith('Character Ascension')).sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const localSpecialties = allMaterials.filter(material => material?.typeText.startsWith('Local')).sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const fish = allMaterials.filter(material => material?.typeText === 'Fish').sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
-const wood = allMaterials.filter(material => material?.category === 'WOOD').sort((a, b) => a.sortRank - b.sortRank).sort((a, b) => a.id - b.id);
+const allMaterials = genshinDb.materials('names', defaultOptions).sort((a, b) => a.sortRank === b.sortRank ? a.id - b.id : a.sortRank - b.sortRank);
+
+const characterAscensionMaterials = allMaterials.filter(material => material?.typeText.startsWith('Character Ascension Material') && !material?.description.startsWith('Character Ascension'));
+const characterLVLMaterials = allMaterials.filter(material => material?.typeText.startsWith('Character Level-Up Material') && !material?.description.startsWith('Character Ascension'));
+const characterWeaponEnhancementMaterials = allMaterials.filter(material => material?.typeText.startsWith('Character and Weapon Enhancement Material'));
+const fish = allMaterials.filter(material => material?.typeText === 'Fish');
+const localSpecialties = allMaterials.filter(material => material?.typeText.startsWith('Local'));
+const talentMaterials = allMaterials.filter(material => material?.typeText.startsWith('Character Talent Material') && !material?.name.startsWith('Crown of Insight'));
+const weaponMaterials = allMaterials.filter(material => material?.typeText.startsWith('Weapon Ascension Material'));
+const wood = allMaterials.filter(material => material?.category === 'WOOD');
 
 const materials = {
 	characterAscensionMaterials,
