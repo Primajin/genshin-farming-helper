83fd8c441111baa0a7dd9962d72acea12218d83d
Jannis Hell <Primajin@users.noreply.github.com>
2023-12-10 17:41:19 +0100

    :test_tube: add test for each increase button and goals

--- src/components/__tests__/farm-helper.test.jsx
+++ src/components/__tests__/farm-helper.test.jsx
@@ -23,11 +23,12 @@ const materialMap = new Map([
 const config = [0, false, 1, false, 2, false, 3];
 const configWithLock = [0, false, 1, true, 2, false, 3];
 const configWithLockPrefilled = [0, false, 4, true, 2, false, 3];
+
+const globalMockRemove = vi.fn();
 describe('FarmHelper', () => {
 	for (const [category, {name, tiers}] of materialMap) {
 		it(`renders correctly for ${category} ${name} with ${tiers} tiers`, () => {
-			const mockRemove = vi.fn();
-			const rendering = render(<FarmHelper category={category} config={config} item={name} materials={materials} onRemove={mockRemove}/>);
+			const rendering = render(<FarmHelper category={category} config={config} item={name} materials={materials} onRemove={globalMockRemove}/>);
 			expect(rendering).toMatchSnapshot();
 
 			const buttons = screen.getAllByTestId(/button-tier-/);
@@ -35,8 +36,7 @@ describe('FarmHelper', () => {
 		});
 
 		it(`increases counter correctly for ${category} ${name} on first tier`, () => {
-			const mockRemove = vi.fn();
-			render(<FarmHelper category={category} config={config} item={name} materials={materials} onRemove={mockRemove}/>);
+			render(<FarmHelper category={category} config={config} item={name} materials={materials} onRemove={globalMockRemove}/>);
 
 			const buttons = screen.getAllByTestId(/button-tier-/);
 			const valuesBefore = screen.getAllByTestId(/value-tier-/).map(element => element.textContent);
@@ -93,8 +93,7 @@ describe('FarmHelper', () => {
 		});
 
 		it(`increases counter correctly for ${category} ${name} when second tier is locked`, () => {
-			const mockRemove = vi.fn();
-			render(<FarmHelper category={category} config={configWithLock} item={name} materials={materials} onRemove={mockRemove}/>);
+			render(<FarmHelper category={category} config={configWithLock} item={name} materials={materials} onRemove={globalMockRemove}/>);
 
 			const buttons = screen.getAllByTestId(/button-tier-/);
 			const valuesBefore = screen.getAllByTestId(/value-tier-/).map(element => element.textContent);
@@ -152,8 +151,7 @@ describe('FarmHelper', () => {
 
 		if (tiers > 1) {
 			it(`correctly tallies for ${category} ${name} when lock is lifted`, async () => {
-				const mockRemove = vi.fn();
-				render(<FarmHelper category={category} config={configWithLockPrefilled} item={name} materials={materials} onRemove={mockRemove}/>);
+				render(<FarmHelper category={category} config={configWithLockPrefilled} item={name} materials={materials} onRemove={globalMockRemove}/>);
 
 				const buttons = screen.getAllByTestId(/button-tier-/);
 				const valuesBefore = screen.getAllByTestId(/value-tier-/).map(element => element.textContent);
@@ -203,6 +201,62 @@ describe('FarmHelper', () => {
 			});
 		}
 	}
+
+	it('increases correctly if every button was clicked sequentially', () => {
+		const {name} = materialMap.get(ASCENSION);
+
+		render(<FarmHelper category={ASCENSION} config={config} item={name} materials={materials} onRemove={globalMockRemove}/>);
+
+		const buttons = screen.getAllByTestId(/button-tier-/);
+		const valuesBefore = screen.getAllByTestId(/value-tier-/).map(element => element.textContent);
+
+		for (let i = 0; i < 4; i++) {
+			fireEvent.click(buttons[i]);
+		}
+
+		const valuesAfter = screen.getAllByTestId(/value-tier-/).map(element => element.textContent);
+
+		expect(valuesBefore).toMatchObject(['0', '1', '2', '3']);
+		expect(valuesAfter).toMatchObject(['1', '2', '0', '5']);
+	});
+
+	it('sets a goal and changes to green when goal is reached', () => {
+		const {name} = materialMap.get(ASCENSION);
+
+		render(<FarmHelper category={ASCENSION} config={config} item={name} materials={materials} onRemove={globalMockRemove}/>);
+
+		const goalLabels = screen.getAllByTestId('goal-label');
+		const buttons = screen.getAllByTestId(/button-tier-/);
+		const valuesBefore = buttons.map(button => button.querySelector('b'));
+
+		for (const className of valuesBefore.map(value => value.className)) {
+			expect(className).toBe('css-0');
+		}
+
+		// Currently the values are     0, 1, 2, 3
+		// and the goals will be set to 0, 1, 2, 3
+		for (let i = 0; i < 4; i++) {
+			const input = goalLabels[i].querySelector('input');
+			fireEvent.change(input, {target: {value: `${i}`}});
+		}
+
+		// Now everything will be increased by one
+		for (let i = 0; i < 4; i++) {
+			fireEvent.click(buttons[i]);
+		}
+		// The values are now         1, 2, 0, 5
+		// so the goals will match on the second and the last item
+
+		const valuesAfter = buttons.map(button => button.querySelector('b'));
+
+		for (const [index, className] of valuesAfter.map(value => value.className).entries()) {
+			if (index % 2) {
+				expect(className).not.toBe('css-0');
+			} else {
+				expect(className).toBe('css-0');
+			}
+		}
+	});
 
 	it('calls remove correctly when button is clicked', () => {
 		const mockRemove = vi.fn();
