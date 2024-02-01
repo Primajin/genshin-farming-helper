56753a397c7a041c532fef5469410efdb9566a6c
Jannis Hell <Primajin@users.noreply.github.com>
2024-02-01 11:27:13 +0000

    update tests

--- setup-tests.js
+++ setup-tests.js
@@ -1,9 +1,11 @@
 // eslint-disable unicorn/filename-case
-import {afterEach} from 'vitest';
+import {afterEach, vi} from 'vitest';
 import {cleanup} from '@testing-library/react';
 import '@testing-library/jest-dom/vitest'; // eslint-disable-line import/no-unassigned-import
 
 // Runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
 	cleanup();
+	vi.clearAllMocks();
+	vi.resetAllMocks();
 });
--- src/components/__tests__/main.test.jsx
+++ src/components/__tests__/main.test.jsx
@@ -1,7 +1,25 @@
 import {render} from '@testing-library/react';
-import {describe, expect, it} from 'vitest';
+import {describe, expect, it, vi} from 'vitest';
 
 import Main from '../main.jsx';
+
+vi.mock('../../data.json', async () => {
+	const {materials} = await vi.importActual('./mock-data.js');
+	return {
+		default: {
+			...materials,
+		},
+	};
+});
+
+vi.mock('../../data-rare.json', async () => {
+	const {materialsRare} = await vi.importActual('./mock-data.js');
+	return {
+		default: {
+			...materialsRare,
+		},
+	};
+});
 
 describe('Main', () => {
 	it('renders without crashing', () => {
