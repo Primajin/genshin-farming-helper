347bd9859c4ece9e083d6e30645a41016bd78f82
Jannis Hell <Primajin@users.noreply.github.com>
2024-02-23 15:43:35 +0100

    :articulated_lorry: pre-populate local-storage when on pr-preview

---
+++ src/utils/__tests__/__mocks__/local-storage.js
@@ -0,0 +1,12 @@
+const localStorageState = {
+	'TALENT.Philosophies of Freedom': [1, false, 2, false, 3, false, 0, '', 2, '', ''],
+	'WEAPON.Scattered Piece of Decarabian\'s Dream': [1, false, 2, false, 3, true, 4, '', '', 3, ''],
+	'ENHANCEMENT.Slime Concentrate': [1, false, 2, false, 3, false, 0, 1, '', '', ''],
+	'ASCENSION.Brilliant Diamond Gemstone': [1, false, 2, false, 3, true, 4, '', '', '', ''],
+	'LEVEL.Hurricane Seed': [1, true, 0, false, 0, false, 0, '', '', '', ''],
+	'LOCAL.Wolfhook': [1, true, 0, false, 0, false, 0, 2, '', '', ''],
+	'FISH.Medaka': [1, true, 0, false, 0, false, 0, '', '', '', ''],
+	'WOOD.Birch Wood': [1, true, 0, false, 0, false, 0, '', '', '', ''],
+};
+
+export default localStorageState;
