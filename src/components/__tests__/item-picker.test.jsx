69bdd397201c4bd6e94fd2765ffb6e06c4bb3ee5
Jannis Hell <Primajin@users.noreply.github.com>
2024-03-04 16:13:15 +0100

    :motorway: remove path

--- src/components/__tests__/item-picker.test.jsx
+++ src/components/__tests__/item-picker.test.jsx
@@ -21,7 +21,7 @@ describe('ItemPicker', () => {
 		expect(label).toBeDefined();
 		const backgroundStyle = label.querySelectorAll('div')[0].getAttribute('style');
 		const regExp = /url\((\S+)\)/g;
-		expect(regExp.exec(backgroundStyle)[1]).toBe('/genshin-farming-helper/src/images/bg/background_Item_5_Star.png');
+		expect(regExp.exec(backgroundStyle)[1]).toBe('/src/images/bg/background_Item_5_Star.png');
 	});
 
 	it('renders with single element (char lvl)', () => {
@@ -30,7 +30,7 @@ describe('ItemPicker', () => {
 		expect(label).toBeDefined();
 		const backgroundStyle = label.querySelectorAll('div')[0].getAttribute('style');
 		const regExp = /url\((\S+)\)/g;
-		expect(regExp.exec(backgroundStyle)[1]).toBe('/genshin-farming-helper/src/images/bg/background_Item_3_Star.png');
+		expect(regExp.exec(backgroundStyle)[1]).toBe('/src/images/bg/background_Item_3_Star.png');
 	});
 
 	it('renders with multiple elements', () => {
@@ -43,7 +43,7 @@ describe('ItemPicker', () => {
 		expect(label3).toBeDefined();
 		const backgroundStyle = label2.querySelectorAll('div')[0].getAttribute('style');
 		const regExp = /url\((\S+)\)/g;
-		expect(regExp.exec(backgroundStyle)[1]).toBe('/genshin-farming-helper/src/images/bg/background_Item_4_Star.png');
+		expect(regExp.exec(backgroundStyle)[1]).toBe('/src/images/bg/background_Item_4_Star.png');
 	});
 
 	it('renders alternative image when initial one can not be loaded', () => {
