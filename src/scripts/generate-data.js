5b37a1fdd8414be7596340cfa554f0392e76e455
Jannis Hell <Primajin@users.noreply.github.com>
2024-07-09 15:28:56 +0200

    :open_umbrella: ignore lines that can't be tested

--- src/scripts/generate-data.js
+++ src/scripts/generate-data.js
@@ -34,6 +34,7 @@ export const materials = {
 };
 
 fs.writeFile('src/data.json', JSON.stringify(materials), error => {
+	/* istanbul ignore if */
 	if (error) {
 		console.error(error);
 	}
@@ -61,6 +62,7 @@ export const materialsRare = {
 };
 
 fs.writeFile('src/data-rare.json', JSON.stringify(materialsRare), error => {
+	/* istanbul ignore if */
 	if (error) {
 		console.error(error);
 	}
