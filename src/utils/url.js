88cf96a95842c833af382ec51157016e5f6a96ca
Jannis Hell <Primajin@users.noreply.github.com>
2024-07-16 15:17:48 +0200

    :test_tube: add tests for url.js

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
 /* global window */
 export const isPRPreview = () => {
-	const {pathname} = window?.location ?? {};
+	const {pathname} = window?.location ?? {pathname: {includes: () => false}};
 	return pathname.includes('/pr-preview');
 };
