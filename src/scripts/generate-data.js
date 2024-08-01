b74d89260063344b196f62631ebbd3bfd5fe7c31
Lint Action <lint-action@samuelmeuli.com>
2024-08-01 08:02:23 +0000

    :sparkles: Fix code style issues with XO

--- src/scripts/generate-data.js
+++ src/scripts/generate-data.js
@@ -34,7 +34,7 @@ export const materials = {
 };
 
 fs.writeFile('src/data.json', JSON.stringify(materials), error => {
-	/* v8 ignore next 3 */
+	/* V8 ignore next 3 */
 	if (error) {
 		console.error(error);
 	}
@@ -62,7 +62,7 @@ export const materialsRare = {
 };
 
 fs.writeFile('src/data-rare.json', JSON.stringify(materialsRare), error => {
-	/* v8 ignore next 3 */
+	/* V8 ignore next 3 */
 	if (error) {
 		console.error(error);
 	}
