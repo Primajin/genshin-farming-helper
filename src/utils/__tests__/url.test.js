fb117a6d7f29da5565e13aad48d4c482ce0f2e01
Jannis Hell <Primajin@users.noreply.github.com>
2024-12-11 15:52:49 +0100

    :package: update dependencies

--- src/utils/__tests__/url.test.js
+++ src/utils/__tests__/url.test.js
@@ -1,11 +1,10 @@
-/* global window */
 import {describe, it, expect} from 'vitest';
 import {isPRPreview} from '../url.js';
 
 describe('isPRPreview', () => {
 	it('should return true if the current URL includes "/pr-preview"', () => {
 		// Mock the window.location object
-		Object.defineProperty(window, 'location', {
+		Object.defineProperty(globalThis, 'location', {
 			value: {
 				pathname: '/some/path/pr-preview',
 			},
@@ -18,14 +17,14 @@ describe('isPRPreview', () => {
 
 	it('should return false if the current URL does not include "/pr-preview"', () => {
 		// Mock the window.location object
-		Object.defineProperty(window, 'location', {value: {pathname: '/some/other/path'}});
+		Object.defineProperty(globalThis, 'location', {value: {pathname: '/some/other/path'}});
 		const result = isPRPreview();
 		expect(result).toBeFalsy();
 	});
 
 	it('should handle the case when window.location is undefined', () => {
 		// Mock the window.location object to be undefined
-		Object.defineProperty(window, 'location', {value: undefined});
+		Object.defineProperty(globalThis, 'location', {value: undefined});
 
 		const result = isPRPreview();
 		expect(result).toBeFalsy();
@@ -33,7 +32,7 @@ describe('isPRPreview', () => {
 
 	it('should handle the case when window in its entirety is undefined', () => {
 		// eslint-disable-next-line no-global-assign
-		window = undefined;
+		globalThis = undefined;
 
 		const result = isPRPreview();
 		expect(result).toBeFalsy();
