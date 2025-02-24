ee6605488f734331b19c3d38dd3479ad4b4c4769
Jannis Hell <Primajin@users.noreply.github.com>
2025-02-24 10:55:17 +0100

    :heavy_plus_sign: Add screen wake lock functionality and related UI controls #356

---
+++ src/utils/wake-lock.js
@@ -0,0 +1,49 @@
+/* global navigator */
+/**
+ * Utility module for managing screen wake locks.
+ * @module wakeLock
+ */
+
+/**
+ * The wake lock object from the navigator, if available.
+ * @type {WakeLock|boolean}
+ */
+const navigatorWakelock = typeof navigator !== 'undefined' && navigator.wakeLock;
+
+/**
+ * The wake lock sentinel.
+ * @type {WakeLockSentinel|null}
+ */
+let wakeLock = null;
+
+/**
+ * Attempts to request a screen wake lock.
+ * @returns {Promise<boolean>} A promise that resolves to true if the wake lock is successfully requested, otherwise false.
+ */
+export const requestWakeLock = async () => {
+	try {
+		wakeLock = await navigatorWakelock.request();
+		wakeLock.addEventListener('release', () => {
+			console.debug('Screen Wake Lock released:', wakeLock.released);
+		});
+		console.debug('Screen Wake Lock requested.');
+		return true;
+	} catch (error) {
+		console.error(`${error.name}, ${error.message}`);
+		return false;
+	}
+};
+
+/**
+ * Releases the screen wake lock if it is currently held.
+ * @returns {boolean} True if the wake lock was released, otherwise false.
+ */
+export const releaseWakeLock = async () => {
+	if (wakeLock) {
+		await wakeLock.release();
+		wakeLock = null;
+		return true;
+	}
+
+	return false;
+};
