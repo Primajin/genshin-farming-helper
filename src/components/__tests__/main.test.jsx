5dd20c8ebd507588e9210bf18d99596a460eb4fe
Jannis Hell <Primajin@users.noreply.github.com>
2025-02-25 15:12:09 +0100

    :test_tube: Add tests for wake lock functionality and update existing test files

--- src/components/__tests__/main.test.jsx
+++ src/components/__tests__/main.test.jsx
@@ -1,6 +1,10 @@
-import {fireEvent, render, screen} from '@testing-library/react';
+/* global navigator */
 import {
-	describe, expect, it, vi,
+	act, fireEvent, render, screen,
+} from '@testing-library/react';
+
+import {
+	beforeEach, describe, expect, test, vi,
 } from 'vitest';
 
 import Main from '../main.jsx';
@@ -24,43 +28,81 @@ vi.mock('../../data-rare.json', async () => {
 });
 
 describe('main', () => {
-	it('renders without crashing', () => {
+	beforeEach(() => {
+		const eventTarget = new EventTarget();
+		const originalNavigator = navigator;
+		global.navigator = {
+			...originalNavigator,
+			wakeLock: {
+				request: vi.fn().mockResolvedValue({
+					addEventListener: eventTarget.addEventListener.bind(eventTarget),
+					dispatchEvent: eventTarget.dispatchEvent.bind(eventTarget),
+					release: vi.fn(),
+					released: 'hello',
+				}),
+			},
+		};
+	});
+
+	test('renders without crashing', () => {
 		const rendering = render(<Main/>);
 		expect(rendering).toMatchSnapshot();
 	});
 
-	it('renders without crashing and all them buttons clicked', () => {
+	test('renders without crashing and all them buttons clicked', async () => {
 		global.innerWidth = 700;
 		global.dispatchEvent(new Event('resize'));
 		const rendering = render(<Main/>);
 
 		// Add the first item
 		const buttons = screen.getAllByTestId('image');
-		fireEvent.click(buttons[0]);
+		await act(() => {
+			fireEvent.click(buttons[0]);
+		});
 		expect(rendering).toMatchSnapshot();
 
 		const floatItems = screen.getByTitle('Click to float items');
-		fireEvent.click(floatItems);
+		await act(() => {
+			fireEvent.click(floatItems);
+		});
 		expect(rendering).toMatchSnapshot();
 
 		const stackItems = screen.getByTitle('Click to stack items');
-		fireEvent.click(stackItems);
+		await act(() => {
+			fireEvent.click(stackItems);
+		});
 		expect(rendering).toMatchSnapshot();
 
 		const removeButton = screen.getByTitle('Remove item');
-		fireEvent.click(removeButton);
+		await act(() => {
+			fireEvent.click(removeButton);
+		});
 		expect(rendering).toMatchSnapshot();
 
 		const fullScreenButton = screen.getByTitle('Make fullscreen');
-		fireEvent.click(fullScreenButton);
+		await act(() => {
+			fireEvent.click(fullScreenButton);
+			global.document.dispatchEvent(new Event('fullscreenchange'));
+		});
 		expect(rendering).toMatchSnapshot();
 
-		// Screen does not actually go into fullscreen mode so there is no off case
+		const exitFullScreenButton = screen.getByTitle('Exit fullscreen');
+		await act(() => {
+			fireEvent.click(exitFullScreenButton);
+			global.document.dispatchEvent(new Event('fullscreenchange'));
+		});
+		expect(rendering).toMatchSnapshot();
 
 		const wakeButton = screen.getByTitle('Keep screen awake');
-		fireEvent.click(wakeButton);
+		await act(() => {
+			fireEvent.click(wakeButton);
+		});
 		expect(rendering).toMatchSnapshot();
 
-		// Screen does not actually go into wake mode so there is no off case
+		const sleepButton = screen.getByTitle('Allow screen to sleep');
+		await act(() => {
+			fireEvent.click(sleepButton);
+		});
+		expect(rendering).toMatchSnapshot();
 	});
 });
--- src/utils/__tests__/wake-lock.test.js
+++ src/utils/__tests__/wake-lock.test.js
@@ -1,46 +1,72 @@
 /* global navigator */
 import {
-	describe, test, expect, vi, afterEach,
+	beforeEach, describe, expect, test, vi,
 } from 'vitest';
 
-import {requestWakeLock, releaseWakeLock} from '../wake-lock.js';
+import {releaseWakeLock, requestWakeLock} from '../wake-lock.js';
 
 describe('wake lock', () => {
-	global.navigator.wakeLock = {
-		request: vi.fn().mockResolvedValue({
-			addEventListener: vi.fn(),
-			release: vi.fn(),
-		}),
-	};
-
-	afterEach(() => {
-		vi.clearAllMocks();
-		vi.resetModules();
+	beforeEach(() => {
+		const eventTarget = new EventTarget();
+		const originalNavigator = navigator;
+		global.navigator = {
+			...originalNavigator,
+			wakeLock: {
+				request: vi.fn().mockResolvedValue({
+					addEventListener: eventTarget.addEventListener.bind(eventTarget),
+					dispatchEvent: eventTarget.dispatchEvent.bind(eventTarget),
+					release: vi.fn(),
+					released: 'hello',
+				}),
+			},
+		};
 	});
 
-	describe('requestWakeLock', () => {
+	describe('request lock', () => {
 		test('should request a wake lock successfully', async () => {
-			const result = await requestWakeLock();
-			expect(result).toBe(true);
+			const consoleSpy = vi.spyOn(console, 'debug');
+			const wakeLockSentinel = null;
+			const result = await requestWakeLock(wakeLockSentinel);
+			// Const sentinelSpy = vi.spyOn(result, 'addEventListener');
+			expect(result).not.toBeNull();
+			// Expect(result.addEventListener).toHaveBeenCalledWith('release', expect.any(Function));
+			expect(consoleSpy).toHaveBeenCalledWith('Screen Wake Lock requested.');
+			result.dispatchEvent(new Event('release'));
+			expect(consoleSpy).toHaveBeenCalledWith('Screen Wake Lock released:', 'hello');
+		});
+
+		test('should handle errors when Wake Lock API is not supported.', async () => {
+			global.navigator.wakeLock = null;
+			const consoleSpy = vi.spyOn(console, 'error');
+			const wakeLockSentinel = null;
+			const result = await requestWakeLock(wakeLockSentinel);
+			expect(result).toBeNull();
+			expect(consoleSpy).toHaveBeenCalledWith('Wake Lock API not supported.');
 		});
 
 		test('should handle errors when requesting a wake lock', async () => {
 			global.navigator.wakeLock.request = vi.fn().mockRejectedValue(new Error('Test error'));
-			const result = await requestWakeLock();
-			expect(result).toBe(false);
+			const consoleSpy = vi.spyOn(console, 'error');
+			const wakeLockSentinel = null;
+			const result = await requestWakeLock(wakeLockSentinel);
+			expect(result).toBeNull();
+			expect(consoleSpy).toHaveBeenCalledWith('Error: Test error');
 		});
 	});
 
-	describe.skip('releaseWakeLock', () => {
+	describe('release lock', () => {
 		test('should release the wake lock successfully', async () => {
-			await requestWakeLock();
-			const result = await releaseWakeLock();
-			expect(result).toBe(true);
+			let wakeLockSentinel = null;
+			wakeLockSentinel = await requestWakeLock(wakeLockSentinel);
+			expect(wakeLockSentinel).not.toBeNull();
+			const result = await releaseWakeLock(wakeLockSentinel);
+			expect(result).toBeNull();
 		});
 
-		test('should return false if there is no wake lock to release', async () => {
-			const result = await releaseWakeLock();
-			expect(result).toBe(false);
+		test('should return null if there is no wake lock to release', async () => {
+			const wakeLockSentinel = null;
+			const result = await releaseWakeLock(wakeLockSentinel);
+			expect(result).toBeNull();
 		});
 	});
 });
