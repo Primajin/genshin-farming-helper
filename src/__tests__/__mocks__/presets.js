42fbce2d3f7915548b30b410121e183e33d3b4ef
Lint Action <Primajin@users.noreply.github.com>
2025-11-09 14:16:05 +0000

    :sparkles: fix code style issues with XO

--- src/__tests__/__mocks__/presets.js
+++ src/__tests__/__mocks__/presets.js
@@ -1,18 +1,18 @@
 export const presets = {
 	characters: [
 		{
-			id: 10000005,
+			id: 10_000_005,
 			name: 'Test Character',
 			element: 'Anemo',
 			rarity: 5,
 			items: [
 				{
-					id: 104101,
+					id: 104_101,
 					name: 'Brilliant Diamond Sliver',
 					count: 1,
 				},
 				{
-					id: 104102,
+					id: 104_102,
 					name: 'Brilliant Diamond Fragment',
 					count: 9,
 				},
@@ -24,13 +24,13 @@ export const presets = {
 	],
 	weapons: [
 		{
-			id: 11101,
+			id: 11_101,
 			name: 'Test Weapon',
 			weaponType: 'Sword',
 			rarity: 4,
 			items: [
 				{
-					id: 114017,
+					id: 114_017,
 					name: 'Mist Veiled Lead Elixir',
 					count: 3,
 				},
