4958081bfd4d95de5cd7525a775ad7f664f7d756
Lint Action <Primajin@users.noreply.github.com>
2025-11-13 17:58:28 +0000

    :sparkles: fix code style issues with XO

--- src/scripts/fishing-rods-data.js
+++ src/scripts/fishing-rods-data.js
@@ -3,49 +3,49 @@
 // This data is not available in genshin-db, so we maintain it manually
 // Using IDs instead of names for more reliable matching
 export const fishingRodRecipes = {
-	200908: [ // Moongleam Sapling
+	200_908: [ // Moongleam Sapling
 		'Common Axehead Fish',
 		'Veggie Mauler Shark',
 		'Azuregaze Crystal-Eye',
 		'Nightgaze Crystal-Eye',
 	],
-	200906: [ // Wavepiercer
+	200_906: [ // Wavepiercer
 		'Neon Mauler Shark',
 		'Dawncatcher',
 		'Purple Shirakodai',
 		'Purple Shirakodai',
 	],
-	200903: [ // Narukawa Ukai
+	200_903: [ // Narukawa Ukai
 		'Medaka',
 		'Glaze Medaka',
 		'Lunged Stickleback',
 		'Purple Shirakodai',
 	],
-	200901: [ // Windtangler
+	200_901: [ // Windtangler
 		'Common Axehead Fish',
 		'Dawncatcher',
 		'Lunged Stickleback',
 		'Azuregaze Crystal-Eye',
 	],
-	200907: [ // Flowhorn Flounderfinder
+	200_907: [ // Flowhorn Flounderfinder
 		'Veggie Mauler Shark',
 		'Neon Mauler Shark',
 		'Nightgaze Crystal-Eye',
 		'Nightgaze Crystal-Eye',
 	],
-	200904: [ // Moonstringer
+	200_904: [ // Moonstringer
 		'Medaka',
 		'True Fruit Angler',
 		'Sunset Cloud Angler',
 		'Sandstorm Angler',
 	],
-	200905: [ // Serendipity
+	200_905: [ // Serendipity
 		'Medaka',
 		'True Fruit Angler',
 		'Sunset Cloud Angler',
 		'Sandstorm Angler',
 	],
-	200902: [ // Wishmaker
+	200_902: [ // Wishmaker
 		'Glaze Medaka',
 		'Purple Shirakodai',
 		'Brown Shirakodai',
