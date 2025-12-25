1ef66abc8fb41331826725f91dee4bbdbced8e25
Jannis Hell <Primajin@users.noreply.github.com>
2025-12-25 13:07:50 +0100

    :fishing_pole_and_fish: Update fishing rod recipes with new targets by character weapon

--- src/scripts/fishing-rods-data.js
+++ src/scripts/fishing-rods-data.js
@@ -3,17 +3,17 @@
 // This data is not available in genshin-db, so we maintain it manually
 // Using fish IDs for reliable matching
 export const fishingRodRecipes = {
-	200_908: [ // Moongleam Sapling
-		131_046, // Common Axehead Fish
-		131_049, // Veggie Mauler Shark
-		131_051, // Azuregaze Crystal-Eye
-		131_052, // Nightgaze Crystal-Eye
+	200_901: [ // Windtangler
+		131_000, // Medaka
+		131_003, // Aizen Medaka
+		131_008, // Venomspine Fish
+		131_015, // Tea-Colored Shirakodai
 	],
-	200_906: [ // Wavepiercer
-		131_050, // Neon Mauler Shark
-		131_004, // Dawncatcher
-		131_014, // Purple Shirakodai
-		131_014, // Purple Shirakodai
+	200_902: [ // Wishmaker
+		131_000, // Medaka
+		131_002, // Sweet-Flower Medaka
+		131_007, // Betta
+		131_013, // Brown Shirakodai
 	],
 	200_903: [ // Narukawa Ukai
 		131_000, // Medaka
@@ -21,34 +21,31 @@ export const fishingRodRecipes = {
 		131_006, // Lunged Stickleback
 		131_014, // Purple Shirakodai
 	],
-	200_901: [ // Windtangler
-		131_046, // Common Axehead Fish
-		131_004, // Dawncatcher
-		131_006, // Lunged Stickleback
-		131_051, // Azuregaze Crystal-Eye
-	],
-	200_907: [ // Flowhorn Flounderfinder
-		131_049, // Veggie Mauler Shark
-		131_050, // Neon Mauler Shark
-		131_052, // Nightgaze Crystal-Eye
-		131_052, // Nightgaze Crystal-Eye
-	],
 	200_904: [ // Moonstringer
-		131_000, // Medaka
-		131_023, // True Fruit Angler
-		131_026, // Sunset Cloud Angler
-		131_025, // Sandstorm Angler
+		// obtained from event, no recipe
 	],
 	200_905: [ // Serendipity
 		131_000, // Medaka
 		131_023, // True Fruit Angler
-		131_026, // Sunset Cloud Angler
 		131_025, // Sandstorm Angler
+		131_026, // Sunset Cloud Angler
 	],
-	200_902: [ // Wishmaker
-		131_001, // Glaze Medaka
-		131_014, // Purple Shirakodai
-		131_013, // Brown Shirakodai
+	200_906: [ // Wavepiercer
+		131_029, // Streaming Axe Marlin
+		131_032, // Maintenance Mek: Initial Configuration
+		131_033, // Maintenance Mek: Water Body Cleaner
+		131_034, // Maintenance Mek: Situation Controller
+	],
+	200_907: [ // Flowhorn Flounderfinder
+		131_040, // Magma Rapidfighting Fish
+		131_041, // Greenwave Sunfish
+		131_042, // Dusk Sunfish
+		131_043, // Phony Phlogiston Unihornfish
+	],
+	200_908: [ // Moongleam Sapling
+		131_046, // Common Axehead Fish
+		131_049, // Veggie Mauler Shark
 		131_051, // Azuregaze Crystal-Eye
+		131_052, // Nightgaze Crystal-Eye
 	],
 };
